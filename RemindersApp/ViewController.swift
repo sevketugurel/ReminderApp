e3464758ea3ce88dc2178b3d9b0a399bddf2cec3
sevketugurel <ugurelsevket@gmail.com>
2023-05-18 19:05:34 +0300

    Initial Commit

---
+++ RemindersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemindersApp
+//
+//  Created by Şevket Uğurel on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
